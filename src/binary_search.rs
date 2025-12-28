fad72d4d27d327464e892156b1fb44866957db27
thestackdev <shanmukeshwar@icloud.com>
2025-12-28 23:33:42 +0530

    initial commit

---
+++ src/binary_search.rs
@@ -0,0 +1,18 @@
+use std::cmp;
+
+pub fn binary_search<T: Ord>(array: &[T], search_key: T) -> Result<usize, usize> {
+    let mut start: usize = 0;
+    let mut end: usize = array.len();
+
+    while start < end {
+        let middle = start + (end - start) / 2;
+
+        match array[middle].cmp(&search_key) {
+            cmp::Ordering::Less => start = middle + 1,
+            cmp::Ordering::Greater => end = middle,
+            cmp::Ordering::Equal => return Ok(middle),
+        }
+    }
+
+    Err(start)
+}
---
+++ src/linked_list_tmp.rs
@@ -0,0 +1,85 @@
+pub struct Node<T> {
+    value: T,
+    next: Option<Box<Node<T>>>,
+}
+
+impl<T> Node<T> {
+    fn new(value: T, next: Option<Box<Node<T>>>) -> Self {
+        Self { value, next }
+    }
+}
+
+struct LinkedList<T> {
+    head: Option<Box<Node<T>>>,
+}
+
+impl<T> LinkedList<T> {
+    fn new() -> Self {
+        Self { head: None }
+    }
+
+    fn push_front(&mut self, value: T) {
+        let new_node = Box::new(Node::new(value, self.head.take()));
+        self.head = Some(new_node);
+    }
+
+    fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|element| {
+            self.head = element.next;
+            element.value
+        })
+    }
+
+    fn push_back(&mut self, value: T) {
+        let new_node = Box::new(Node::new(value, None));
+        match &mut self.head {
+            None => self.head = Some(new_node),
+            Some(current) => {
+                let mut current = current;
+                while current.next.is_some() {
+                    current = current.next.as_mut().unwrap();
+                }
+
+                current.next = Some(new_node);
+            }
+        }
+    }
+
+    fn len(&self) -> usize {
+        let mut count = 0;
+        let mut current = &self.head;
+
+        while let Some(node) = current {
+            count += 1;
+            current = &node.next;
+        }
+
+        count
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.value)
+    }
+
+    fn is_empty(&self) -> bool {
+        self.head.is_none()
+    }
+
+    fn pop_back(&mut self) -> Option<T> {
+        if self.head.is_none() {
+            return None;
+        }
+
+        if self.head.as_ref().unwrap().next.is_none() {
+            return self.pop_front();
+        }
+
+        let mut current = self.head.as_mut().unwrap();
+
+        while current.next.as_ref().unwrap().next.is_some() {
+            current = current.next.as_mut().unwrap();
+        }
+
+        current.next.take().map(|node| node.value)
+    }
+}
